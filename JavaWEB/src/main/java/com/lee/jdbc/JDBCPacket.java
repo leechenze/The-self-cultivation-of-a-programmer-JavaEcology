a0c9ad6907ea6647a328ab6669fc7c3591f708e1
leechenze <leeczyc@gmail.com>
2023-01-03 22:44:14 +0800

    P031T00:00

---
+++ JavaWEB/src/main/java/com/lee/jdbc/JDBCPacket.java
@@ -0,0 +1,29 @@
+package com.lee.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class JDBCPacket {
+    public static void main(String[] args) throws Exception {
+        // 1。注册驱动
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        // 2。获取连接
+        String url = "jdbc:mysql://127.0.0.1:3306/jdbc_packer";
+        String userName = "root";
+        String password = "lcz19930316";
+        Connection conn = DriverManager.getConnection(url, userName, password);
+        // 3。定义sql语句
+        String sql = "update account set money = 1000 where id = 1;";
+        // 4。获取执行sql的对象
+        Statement stmt = conn.createStatement();
+        // 5。执行sql(受影响的行数 Affected rows: 1)
+        int count = stmt.executeUpdate(sql);
+        // 6.打印处理结果
+        System.out.println(count);
+        // 7.释放资源
+        stmt.close();
+        conn.close();
+
+    }
+}
