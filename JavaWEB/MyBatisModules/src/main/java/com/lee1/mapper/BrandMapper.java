50b6108882a558b66b2b0b7b96d665bb7d7806b6
leechenze <leeczyc@gmail.com>
2023-02-02 21:51:33 +0800

    P053T000351

---
+++ JavaWEB/MyBatisModules/src/main/java/com/lee1/mapper/BrandMapper.java
@@ -0,0 +1,14 @@
+package com.lee1.mapper;
+
+import com.lee1.pojo.Brand;
+
+import java.util.List;
+
+public interface BrandMapper {
+
+    /**
+     * 查询所有
+     */
+    public List<Brand> selectAll();
+
+}
