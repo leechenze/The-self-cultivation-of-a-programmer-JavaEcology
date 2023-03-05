27259c19a05dc41639ff2b0b7cc2f799b23921c2
leechenze <leeczyc@gmail.com>
2023-03-05 23:10:41 +0800

    P055T000000

---
+++ Spring/springmvc_restful/src/main/java/com/lee/config/SpringMvcConfig.java
@@ -0,0 +1,15 @@
+package com.lee.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.Filter;
+import javax.servlet.annotation.WebFilter;
+
+@Configuration
+@ComponentScan("com.lee.controller")
+@EnableWebMvc
+public class SpringMvcConfig {
+}
