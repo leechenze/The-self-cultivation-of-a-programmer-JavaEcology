0d04da2c6ef542616a85752f6233418476ba9fb4
leechenze <leeczyc@gmail.com>
2023-03-07 23:28:23 +0800

    P061T000402

--- Spring/springmvc_ssm/src/main/java/com/lee/config/SpringConfig.java
+++ Spring/springmvc_ssm/src/main/java/com/lee/config/SpringConfig.java
@@ -5,11 +5,10 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan({"com.lee.service"})
-@PropertySource("jdbc.properties")
+@PropertySource("classpath:jdbc.properties")
 @Import({JdbcConfig.class, MyBatisConfig.class})
 public class SpringConfig {
 
---
+++ Spring/springmvc_ssm/src/main/java/com/lee/service/BookService.java
@@ -0,0 +1,44 @@
+package com.lee.service;
+
+import com.lee.domain.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public interface BookService {
+
+    /**
+     * 保存
+     * @param book
+     * @return
+     */
+    public boolean save(Book book);
+
+    /**
+     *修改
+     * @param book
+     * @return
+     */
+    public boolean update(Book book);
+
+    /**
+     * 根据ID删除
+     * @param id
+     * @return
+     */
+    public boolean delete(Integer id);
+
+    /**
+     * 根据ID查询
+     * @param id
+     * @return
+     */
+    public Book getById(Integer id);
+
+    /**
+     * 查询全部
+     * @return
+     */
+    public List<Book> getAll();
+}
