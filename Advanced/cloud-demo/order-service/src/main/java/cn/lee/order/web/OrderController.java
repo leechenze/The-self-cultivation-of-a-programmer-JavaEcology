dfe553ea071c146133e44fb0245fba10f33ad0ce
leechenze <leeczyc@gmail.com>
2023-05-16 22:26:13 +0800

    P145T000000

--- Advanced/cloud-demo/order-service/src/main/java/cn/lee/order/web/OrderController.java
+++ Advanced/cloud-demo/order-service/src/main/java/cn/lee/order/web/OrderController.java
@@ -2,6 +2,7 @@ package cn.lee.order.web;
 
 import cn.lee.order.pojo.Order;
 import cn.lee.order.service.OrderService;
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,6 +16,8 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    // 热点参数限流对默认的SpringMVC资源无效，只有通过@SentinelResource注解声明的资源才可以配置热点参数限流。
+    @SentinelResource("hot")
     @GetMapping("{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
